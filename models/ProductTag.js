d66e44cf46e4247c04d95dedf99bb7458a43aa2e
lilys0 <lsouryaphanh@gmail.com>
2021-11-17 14:42:21 +1100

    foreign key set up

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,11 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      // references product model id
+      references: {
+        model: "product",
+        key: "id",
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      // references tag model id
+      references: {
+        model: "tag",
+        key: "id",
+      },
     },
   },
   {
